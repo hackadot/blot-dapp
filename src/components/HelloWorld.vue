3a5c142fb3d3f85b05756b73d2df8e97718b492b
AKO <ako@AnyMacs-MacBook-Pro.local>
2021-06-20 12:33:10 +0300

    call initWeb3 from component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,26 +5,24 @@
 </template>
 
 <script>
-import { web3Accounts, web3Enable } from '@polkadot/extension-dapp'
+import { mapActions } from 'vuex'
+
   export default {
     name: 'HelloWorld',
 
     data(){
       return {}
     },
-    methods:{},
+    methods:{
+      ...mapActions(['initWeb3'])
+    },
+  
      async mounted () {
     // this call fires up the authorization popup
-    const extensions = await web3Enable('my cool dapp');
-    if (extensions.length === 0) {
-        // no extension installed, or the user did not accept the authorization
-        // in this case we should inform the use and give a link to the extension
-        return;
-    }
+  this.initWeb3();
     // we are now informed that the user has at least one extension and that we
     // will be able to show and use accounts
-    const allAccounts = await web3Accounts();
-    console.log(allAccounts)
+   
   }
   }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,29 @@ import Vue from 'vue';
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
+import { web3Accounts, web3Enable } from '@polkadot/extension-dapp'
 
 export default new Vuex.Store({
     state: {},
-    mutations: {},
+    mutations: {
+
+
+    },
     getters: {},
-    actions: {},
+    actions: {
+      async initWeb3() {
+        const extensions = await web3Enable('my cool dapp');  
+        if (extensions.length === 0) {
+          // no extension installed, or the user did not accept the authorization
+          // in this case we should inform the use and give a link to the extension
+          return;
+      }
+
+      const allAccounts = await web3Accounts();
+      console.log(allAccounts)
+      
+      }
+
+
+    },
   })
