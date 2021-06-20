49028eda06c3482211f47ff89912c9cd709f7ba4
vldmkr <vladmakaryan@gmail.com>
2021-06-20 10:40:53 +0300

    polkadot/extension-dapp added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,10 +31,28 @@
 </template>
 
 <script>
+import { web3Accounts, web3Enable } from '@polkadot/extension-dapp'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  async mounted () {
+    // this call fires up the authorization popup
+    const extensions = await web3Enable('my cool dapp');
+
+    if (extensions.length === 0) {
+        // no extension installed, or the user did not accept the authorization
+        // in this case we should inform the use and give a link to the extension
+        return;
+    }
+
+    // we are now informed that the user has at least one extension and that we
+    // will be able to show and use accounts
+    const allAccounts = await web3Accounts();
+    console.log(allAccounts)
   }
 }
 </script>
