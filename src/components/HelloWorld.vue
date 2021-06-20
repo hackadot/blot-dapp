75519fe5d7275c29f5414c4839ca03c975e0ac3e
vldmkr <vladmakaryan@gmail.com>
2021-06-20 12:26:34 +0300

    use blot.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,17 +5,22 @@
 </template>
 
 <script>
-import { web3Accounts, web3Enable } from '@polkadot/extension-dapp'
+  import { web3Enable, web3Accounts } from 'blot.js'
   export default {
     name: 'HelloWorld',
 
     data(){
       return {}
     },
+
     methods:{},
+
      async mounted () {
     // this call fires up the authorization popup
-    const extensions = await web3Enable('my cool dapp');
+
+    const extensions = await web3Enable('Hackathon Token');
+
+    console.log('extensions', extensions)
     if (extensions.length === 0) {
         // no extension installed, or the user did not accept the authorization
         // in this case we should inform the use and give a link to the extension
